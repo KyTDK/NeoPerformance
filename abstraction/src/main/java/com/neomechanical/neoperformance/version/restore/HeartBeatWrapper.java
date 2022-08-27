1808584f0dd7597c76c3cd8f09b19f1a3ef309c6
KyTDK <71435411+KyTDK@users.noreply.github.com>
2022-08-27 20:57:37 +1000

    v1.13 versioning

--- abstraction/src/main/java/com/neomechanical/neoperformance/version/restore/HeartBeatWrapper.java
+++ abstraction/src/main/java/com/neomechanical/neoperformance/version/restore/HeartBeatWrapper.java
@@ -1,7 +1,5 @@
 package com.neomechanical.neoperformance.version.restore;
 
-import com.neomechanical.neoperformance.performanceOptimiser.halt.CachedData;
-import com.neomechanical.neoperformance.performanceOptimiser.managers.DataManager;
 import com.neomechanical.neoutils.version.VersionWrapper;
 
 public interface HeartBeatWrapper extends VersionWrapper {
