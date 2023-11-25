dbce5515f53ac4e8b6dd66e0ff83b31d6b1e1136
KyTDK <71435411+KyTDK@users.noreply.github.com>
2023-11-25 15:52:08 +1100

    - Use book for message fixes

--- api/src/main/java/com/neomechanical/neoperformance/commands/insights/InsightsFixCommand.java
+++ api/src/main/java/com/neomechanical/neoperformance/commands/insights/InsightsFixCommand.java
@@ -7,6 +7,7 @@ import com.neomechanical.neoperformance.performance.modules.insight.InsightManag
 import com.neomechanical.neoperformance.performance.modules.insight.elements.InsightElement;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -40,6 +41,24 @@ public class InsightsFixCommand extends Command {
         return false;
     }
 
+    @NotNull
+    private static AtomicBoolean fixElements(CommandSender commandSender) {
+        HashMap<String, HashMap<String, InsightElement<?>>> insightMap = new InsightManager().getInsightsMap();
+
+        AtomicBoolean fixedElement = new AtomicBoolean(false);
+        insightMap.forEach((category, categoryInsights) ->
+                categoryInsights.entrySet().stream()
+                        .filter(entry -> entry.getValue().isAutomatic)
+                        .forEach(entry -> {
+                            fixedElement.set(true);
+                            InsightElement<?> insightElement = entry.getValue();
+                            insightElement.fix((Player) commandSender);
+                            MessageUtil.sendMM(commandSender, NeoPerformance.getLanguageManager().getString("insights.fixed", null) + " " + entry.getKey());
+                        })
+        );
+        return fixedElement;
+    }
+
     @Override
     public void perform(CommandSender commandSender, String[] strings) {
         if (strings.length < 3) {
@@ -48,19 +67,7 @@ public class InsightsFixCommand extends Command {
         }
         String categoryObtained = strings[2];
         if (categoryObtained.equals("all")) {
-            HashMap<String, HashMap<String, InsightElement<?>>> insightMap = new InsightManager().getInsightsMap();
-
-            AtomicBoolean fixedElement = new AtomicBoolean(false);
-            insightMap.forEach((category, categoryInsights) ->
-                    categoryInsights.entrySet().stream()
-                            .filter(entry -> entry.getValue().isAutomatic)
-                            .forEach(entry -> {
-                                fixedElement.set(true);
-                                InsightElement<?> insightElement = entry.getValue();
-                                insightElement.fix((Player) commandSender);
-                                MessageUtil.sendMM(commandSender, NeoPerformance.getLanguageManager().getString("insights.fixed", null) + " " + entry.getKey());
-                            })
-            );
+            AtomicBoolean fixedElement = fixElements(commandSender);
             if (!fixedElement.get()) {
                 MessageUtil.sendMM(commandSender, NeoPerformance.getLanguageManager().getString("insights.noElements", null));
                 return;
